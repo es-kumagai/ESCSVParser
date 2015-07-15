7233d81c6ee48e16a9e95fe1253ea8a953ae6c67
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-07-15 20:54:07 +0900

    Initial Commit

---
+++ ESCSVParser/ESCSVParser.h
@@ -0,0 +1,19 @@
+//
+//  ESCSVParser.h
+//  ESCSVParser
+//
+//  Created by Tomohiro Kumagai on H27/07/15.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ESCSVParser.
+FOUNDATION_EXPORT double ESCSVParserVersionNumber;
+
+//! Project version string for ESCSVParser.
+FOUNDATION_EXPORT const unsigned char ESCSVParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESCSVParser/PublicHeader.h>
+
+
